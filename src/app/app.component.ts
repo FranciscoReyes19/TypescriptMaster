cd9b74f57735aa260fea4055d2c0eedbc31460b9
FranciscoReyes <freyes@bwl.com.mx>
2020-07-26 21:21:54 -0500

    pipesAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+  arreglo             :number[]       = [1,2,3,4,5,6,7,8,9,10];
+  arregloString       :string[]       = ["hola","mundo"];
+  arregloAny          :any[]          = ["hola",1.56,"mundo"]
+
+  nombre        :string         = "Capitan america";
+
+  PI            :number         = Math.PI;
+  Porcentaje: number = 0.231;
+  salario :number = 1234.5;
+
+  gabriel: Person = {
+    id : 1,
+    name :  'Gabriel',
+    surname : 'Hernadez',
+    birthday: new Date(1993, 8, 19)
+  };
+  constructor(){
+    console.log(this.gabriel);
+  }
+  //Tipado de un objeto
+  
 }
+
+interface Person {
+  id : number;
+  name : string;
+  surname : string;
+  birthday: Date;
+}
+
